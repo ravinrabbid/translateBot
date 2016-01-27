6673134c3159215754dbecb1a790ff0d6d94afca
Frederik Walk <frederik@walk.onl>
2016-01-27 11:35:10 +0100

    Uses stream API, replies to tweets

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	// "errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/BurntSushi/toml"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
+
+type botConfig struct {
+	TranslatedUser string `toml:"translatedUser"`
+	SourceLanguage string `toml:"sourceLanguage"`
+	TargetLanguage string `toml:"targetLanguage"`
+}
 
 type twitterAuth struct {
 	ConsumerKey    string `toml:"consumerKey"`
@@ -22,6 +32,7 @@ type twitterAuth struct {
 }
 
 type config struct {
+	Bot     botConfig   `toml:"bot"`
 	Twitter twitterAuth `toml:"twitter"`
 }
 
@@ -29,14 +40,17 @@ type translation struct {
 	Response struct {
 		Text string `json:"translatedText"`
 	} `json:"responseData"`
+	// Status string `json:"responseStatus"`
 }
 
 var configFile = flag.String("config", "", "Path to configuration file.")
 var conf config
 
-var translateURL = "http://api.mymemory.translated.net/get?langpair=ja|de&q="
+var translateBaseURL = "http://api.mymemory.translated.net/get?"
+var translateURL = ""
 
-func getTranslation(s string) string {
+//TODO strip urls/usernames
+func getTranslation(s string) (string, error) {
 	resp, err := http.Get(translateURL + url.QueryEscape(s))
 	if err != nil {
 		log.Fatalf("Could not reach translation service: %v", err.Error())
@@ -52,7 +66,13 @@ func getTranslation(s string) string {
 		log.Fatalf("Could not parse translation: %v", err.Error())
 	}
 
-	return trans.Response.Text
+	// if trans.Status != "200" {
+	// 	return "", errors.New(trans.Response.Text)
+	// }
+
+	text, _ := url.QueryUnescape(trans.Response.Text) //TODO make this work
+
+	return text, nil
 }
 
 func main() {
@@ -66,18 +86,44 @@ func main() {
 		log.Fatalf("Could not read config: %v", err.Error())
 	}
 
+	translateURL = translateBaseURL + "langpair=" + conf.Bot.SourceLanguage + "|" + conf.Bot.TargetLanguage + "&q="
+
 	oauthConfig := oauth1.NewConfig(conf.Twitter.ConsumerKey, conf.Twitter.ConsumerSecret)
 	oauthToken := oauth1.NewToken(conf.Twitter.AccessKey, conf.Twitter.AccessSecret)
 
 	httpClient := oauthConfig.Client(oauth1.NoContext, oauthToken)
 	client := twitter.NewClient(httpClient)
 
-	tweets, _, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{})
-	if err != nil {
-		log.Fatalf("Could not retrieve timeline: %v", err.Error())
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		log.Printf("Got tweet: '%v' by %v", tweet.Text, tweet.User.ScreenName)
+		//TODO omit retweets without comment
+		if tweet.User.ScreenName == conf.Bot.TranslatedUser && tweet.InReplyToStatusID == 0 {
+			log.Print("Translating...")
+			translation, err := getTranslation(tweet.Text)
+			if err != nil {
+				log.Printf("Translation failed: %v", err.Error())
+			} else {
+				client.Statuses.Update("@"+tweet.User.Name+" "+translation, &twitter.StatusUpdateParams{InReplyToStatusID: tweet.ID})
+			}
+		}
 	}
 
-	for _, tweet := range tweets {
-		fmt.Printf("%v\n=====================\n", tweet.Text)
+	userParams := &twitter.StreamUserParams{
+		StallWarnings: twitter.Bool(true),
+		With:          "followings",
 	}
+	stream, err := client.Streams.User(userParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go demux.HandleChan(stream.Messages)
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	fmt.Println("Stopping Stream...")
+	stream.Stop()
 }
